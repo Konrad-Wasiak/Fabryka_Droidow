d558a5f3472a779b0a2c9ae002b398cf79f505a2
Manuela Walczak <kolo44876@gmail.com>
2024-06-11 16:45:02 +0200

    do klasy batch dodano funkcje szkolenia, malowania i update softu

--- batch.py
+++ batch.py
@@ -8,3 +8,19 @@ class Batch:
         self.wersja_oprogramowania: str = wersja_oprogramowania
         self.rodzaj_klasy: str = rodzaj_klasy
         self.kolor: str = kolor
+
+    def szkol_droidy(self, nowa_umiejetnosc: str) -> None:
+        if nowa_umiejetnosc not in self.umiejetnosci_specjalne:
+            self.umiejetnosci_specjalne.append(nowa_umiejetnosc)
+            print(f"Umiejętność {nowa_umiejetnosc} została dodana do droidów w batchu {self.nazwa}.")
+        else:
+            print(f"Umiejętność {nowa_umiejetnosc} już istnieje w batchu {self.nazwa}.")
+
+    def maluj_droidy(self, nowy_kolor: str) -> None:
+        self.kolor = nowy_kolor
+        print(f"Droidy w batchu {self.nazwa} zostały pomalowane na kolor {nowy_kolor}.")
+
+    def updatuj_software(self, nowa_wersja_oprogramowania: str) -> None:
+        self.wersja_oprogramowania = nowa_wersja_oprogramowania
+        print(
+            f"Wersja oprogramowania droidów w batchu {self.nazwa} została zaktualizowana do {nowa_wersja_oprogramowania}.")

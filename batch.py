dd13e8d1b96f6f1e2b25b4e9d700f48ce041f3e8
Manuela Walczak <kolo44876@gmail.com>
2024-06-11 16:43:43 +0200

    poprawiona klasa batch

--- batch.py
+++ batch.py
@@ -1,8 +1,10 @@
+from typing import List
+
 class Batch:
-    def __init__(self, nazwa, numer_produkcji, ilosc_droidow, umiejetnosci_specjalne, wersja_oprogramowania, rodzaj_klasy, kolor):
-        self.nazwa = f"{nazwa}_{numer_produkcji}"
-        self.ilosc_droidow = ilosc_droidow
-        self.umiejetnosci_specjalne = umiejetnosci_specjalne
-        self.wersja_oprogramowania = wersja_oprogramowania
-        self.rodzaj_klasy = rodzaj_klasy
-        self.kolor = kolor
+    def __init__(self, nazwa: str, numer_produkcji: int, ilosc_droidow: int, umiejetnosci_specjalne: List[str], wersja_oprogramowania: str, rodzaj_klasy: str, kolor: str):
+        self.nazwa: str = f"{nazwa}_{numer_produkcji}"
+        self.ilosc_droidow: int = ilosc_droidow
+        self.umiejetnosci_specjalne: List[str] = umiejetnosci_specjalne
+        self.wersja_oprogramowania: str = wersja_oprogramowania
+        self.rodzaj_klasy: str = rodzaj_klasy
+        self.kolor: str = kolor
